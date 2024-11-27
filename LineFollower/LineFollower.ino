c1cdfb4b3fd6863531edb532c76ca2eefdb93558
maxzetina <maxzetina4@gmail.com>
2024-11-27 15:59:09 -0500

    code from wed lab

--- LineFollower/LineFollower.ino
+++ LineFollower/LineFollower.ino
@@ -5,15 +5,17 @@ const int STDBY = 8;
 const int BIN1 = 7;  // Pololu drive B
 const int BIN2 = 6;
 const int PWMB = 5;
-float Kp = 1.0;
-float Kd = .3;
+float Kp = 1.0 * 255;
+float Kd = 0.1 * 255;
+float Ki = 0.025*255;
 int speedL = 10;
 int speedR = 10;
 float prevE = 0;
 float prevT = 0;
 float alpha = .6;
 float filt_e = 0;
-float slow = .4;
+float totalE = 0;
+float slow = 0.8;
 
 void setup() {
   Serial.begin(115200);
@@ -34,27 +36,37 @@ void setup() {
 
 void loop() {
   float t1 = millis();
-  int sensorRight = map(analogRead(A1), 100, 1000, 0, 255);
-  int sensorMid = map(analogRead(A2), 100, 1000, 0, 255);
-  int sensorLeft = map(analogRead(A3), 100, 1000, 0, 255);
-  float error = 1.0 * (sensorRight - sensorLeft);
-  float dError = (error - prevE) / (t1 - prevT);
+  int sensorRight = constrain(map(analogRead(A1), 550, 915, 0, 255), 1, 255);
+  int sensorMid = constrain(map(analogRead(A2), 550, 915, 0, 255), 1, 255);
+  int sensorLeft = constrain(map(analogRead(A3), 550, 915, 0, 255), 1, 255);
+  float error = (1.0 * (sensorRight - sensorLeft))/(max(sensorRight, sensorLeft));
   filt_e = error * alpha + (1 - alpha) * filt_e;
+  float dError = (filt_e - prevE) / (1.0*t1 - prevT);
   prevE = filt_e;
-  prevT = t1;
+  totalE += filt_e;
 
-  // Serial.print(sensorRight);
-  // Serial.print(sensorMid);
-  Serial.println(error);
-  if (error < 0) {
-    speedL = 255 - (error * Kp + Kd * dError);
+  totalE = constrain(totalE, -100/255, 100/255);
+
+
+  Serial.print(sensorRight);
+  Serial.print(" ");
+  Serial.print(sensorLeft);
+  Serial.print(" ");
+  Serial.print(filt_e);
+  if (filt_e < 0) {
+    speedL = 255 + (filt_e * Kp + Kd * dError + Ki * totalE*(t1 - prevT));
     speedR = 255;
   } else {
     speedL = 255;
-    speedR = 255 + (error * Kp + Kd * dError);
+    speedR = 255 - (filt_e * Kp + Kd * dError + Ki * totalE*(t1 - prevT));
   }
   speedL = constrain(speedL, 0, 255);
   speedR = constrain(speedR, 0, 255);
+  Serial.print(" ");
+  Serial.print(speedL);
+  Serial.print(" ");
+  Serial.println(speedR);
+  prevT = t1;
   drive(speedL * slow, speedR * slow);  //drive forwards
 }
 
@@ -70,6 +82,6 @@ void motorWrite(int spd, int pin_IN1, int pin_IN2, int pin_PWM) {
 }
 
 void drive(int speedL, int speedR) {
-  motorWrite(-speedL, AIN1, AIN2, PWMA);
-  motorWrite(speedR * 0.935, BIN1, BIN2, PWMB);
+  motorWrite(-speedL * 0.94, AIN1, AIN2, PWMA);
+  motorWrite(speedR, BIN1, BIN2, PWMB);
 }
